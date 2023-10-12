a2ac762910938ef30b66a0e45a78f5350a3800fe
Kadyr Bakdaulet <qadyr.bagdaulet@gmail.com>
2023-10-12 10:53:18 +0600

    Stadion/urls.py: not finished pathes commentet

--- Stadion/matches/views.py
+++ Stadion/matches/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+def indexPage(request):
+    template = loader.get_template('about.html')
+    return HttpResponse(template.render())
+
